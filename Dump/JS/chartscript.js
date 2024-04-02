e8e9ab00e8a913ea8f9a98edd3a18a817357c6eb
shafowan <shafowan@users.noreply.github.com>
2024-04-02 20:24:35 +0200

    Admin page files
    Ive sent them as a dump for you to put them in the correct directory in case i mess up something

---
+++ Dump/JS/chartscript.js
@@ -0,0 +1,23 @@
+// Source: w3 schools   https://www.w3schools.com/js/tryit.asp?filename=trychartjs_bars_colors_more 
+
+const xValues = ["Chrome", "Firefox", "Edge", "Other"];
+const yValues = [12, 5, 3, 4];
+const barColors = ["#fff18f", "#de425b","#353c8f","#488f31"];
+
+new Chart("myChart", {
+  type: "bar",
+  data: {
+    labels: xValues,
+    datasets: [{
+      backgroundColor: barColors,
+      data: yValues
+    }]
+  },
+  options: {
+    legend: {display: false},
+    title: {
+      display: true,
+      text: "Browsers Used"
+    }
+  }
+});

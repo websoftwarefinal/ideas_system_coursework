b32ba79d54d4306a1ee7765daf40256cc3833536
KhumboMunsaka <103140158+KhumboMunsaka@users.noreply.github.com>
2024-04-16 20:13:09 +0200

    Adding the Username

--- ewd_database.sql
+++ ewd_database.sql
@@ -22,6 +22,7 @@ CREATE TABLE Department (
 -- Create Staff table
 CREATE TABLE Staff (
     staff_id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL,
     first_name VARCHAR(50) NOT NULL,
     last_name VARCHAR(50) NOT NULL,
     email_address VARCHAR(255) NOT NULL,
